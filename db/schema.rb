529f487b0c1610d27d41771904477885d3422643
tsumichan <kdy78022@gmail.com>
2018-06-26 11:44:46 +0900

    user_idカラムを削除した

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_22_054808) do
+ActiveRecord::Schema.define(version: 2018_06_26_024254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.integer "user_id", null: false
     t.string "title", null: false
     t.string "description"
     t.datetime "due_at"
