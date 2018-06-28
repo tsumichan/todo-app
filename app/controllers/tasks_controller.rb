98b792bae4b812ae9c477d3b833c9091b70e481c
tsumichan <kdy78022@gmail.com>
2018-06-28 16:11:17 +0900

    更新ができるようにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,6 +24,15 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def task_params
