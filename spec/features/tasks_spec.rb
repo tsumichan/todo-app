357f2baab1bdb0b9e1026327a0e3d18d9607137d
tsumichan <kdy78022@gmail.com>
2018-07-02 14:56:35 +0900

    更新時のフラッシュメッセージのテストを追加した

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 describe 'タスク' do
+  before do
+    @task = Task.create(
+      title: 'テスト用タスク', description: 'テスト用タスクです',
+      due_at: '2020-01-01 00:00:00', status:1, priority: 1
+    )
+  end
+
   context '新規のタスクを作成する' do
     it '作成完了のフラッシュメッセージが表示される' do
       visit 'tasks/new'
@@ -11,6 +18,15 @@ describe 'タスク' do
       fill_in '優先度', with: '1'
       click_button '登録'
       expect(page).to have_content 'タスクを追加しました'
+    end
+  end
+
+  context '既存のタスクを更新する' do
+    it '作成完了のフラッシュメッセージが表示される' do
+      visit edit_task_path(@task)
+      fill_in 'タスク名', with: '変更タスク'
+      click_button '登録'
+      expect(page).to have_content 'タスクを更新しました'
     end
   end
 end
