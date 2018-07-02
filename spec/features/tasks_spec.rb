fc255d50401effa76becba5d41d3bc3aa7c153eb
tsumichan <kdy78022@gmail.com>
2018-07-02 14:22:57 +0900

    フラッシュメッセージのテストを追加した

---
+++ spec/features/tasks_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'タスク' do
+  context '新規のタスクを作成する'
+    it '作成完了のフラッシュメッセージが表示される'
+      visit 'tasks/new'
+      fill_in 'タスク名', with: '新規タスク'
+      fill_in 'タスクの説明', with: '新規タスクです'
+      fill_in '期限', with: '2020-01-01 00:00:00'
+      fill_in 'ステータス', with: '1'
+      fill_in '優先度', with: '1'
+      click_button '登録'
+      expect(page).to have_content 'タスクを作成しました'
+    end
+  end
+end
