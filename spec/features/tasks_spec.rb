c3143359fcad07bf40241baa33b63b1bfe6d6160
tsumichan <kdy78022@gmail.com>
2018-07-02 15:20:28 +0900

    タスクが更新されることのテストを追加

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -9,7 +9,7 @@ describe 'タスク' do
   end
 
   context '新規のタスクを作成する' do
-    it 'タスクを作成できる' do
+    it '作成する' do
       visit 'tasks/new'
       fill_in 'タスク名', with: '新規タスク1'
       fill_in 'タスクの説明', with: '新規タスク1です'
@@ -33,9 +33,15 @@ describe 'タスク' do
   end
 
   context '既存のタスクを更新する' do
+    it '変更する' do
+      visit visit edit_task_path(@task)
+      fill_in 'タスク名', with: '変更タスク1'
+      click_button '登録'
+      expect(page).to have_content '変更タスク1'      
+    end
     it '作成完了のフラッシュメッセージが表示される' do
       visit edit_task_path(@task)
-      fill_in 'タスク名', with: '変更タスク'
+      fill_in 'タスク名', with: '変更タスク2'
       click_button '登録'
       expect(page).to have_content 'タスクを更新しました'
     end
