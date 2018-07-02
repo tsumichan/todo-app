bd92faa11d9954d6184aee5d0423cbf15b17eccd
tsumichan <kdy78022@gmail.com>
2018-07-02 14:47:25 +0900

    作成じゃなくて追加だった

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe 'タスク' do
-  context '新規のタスクを作成する'
-    it '作成完了のフラッシュメッセージが表示される'
+  context '新規のタスクを作成する' do
+    it '作成完了のフラッシュメッセージが表示される' do
       visit 'tasks/new'
       fill_in 'タスク名', with: '新規タスク'
       fill_in 'タスクの説明', with: '新規タスクです'
@@ -10,7 +10,7 @@ describe 'タスク' do
       fill_in 'ステータス', with: '1'
       fill_in '優先度', with: '1'
       click_button '登録'
-      expect(page).to have_content 'タスクを作成しました'
+      expect(page).to have_content 'タスクを追加しました'
     end
   end
 end
