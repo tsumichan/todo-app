7c2b7d35069b3095612d3ea6795c8274aa38fb54
tsumichan <kdy78022@gmail.com>
2018-07-03 15:54:10 +0900

    新規タスク1がいっぱいいたのでスマートにした

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,23 +1,24 @@
 require 'rails_helper'
 
 describe 'タスク' do
+  let(:title) { 'テスト用タスク' }
   context '新規のタスクを作成する' do
     it '作成する' do
       visit '/tasks/new'
-      fill_in 'タスク名', with: '新規タスク1'
-      fill_in 'タスクの説明', with: '新規タスク1です'
+      fill_in 'タスク名', with: :title
+      fill_in 'タスクの説明', with: 'テスト用タスクです'
       fill_in '期限', with: '2020-01-01 00:00:00'
       fill_in 'ステータス', with: '1'
       fill_in '優先度', with: '1'
       click_button '登録'
-      expect(Task.exists?(title: '新規タスク1')).to be true
-      expect(page).to have_content '新規タスク1'
+      expect(Task.exists?(title: :title)).to be true
+      expect(page).to have_content :title
     end
 
     it '作成完了のフラッシュメッセージが表示される' do
       visit '/tasks/new'
-      fill_in 'タスク名', with: '新規タスク2'
-      fill_in 'タスクの説明', with: '新規タスク2です'
+      fill_in 'タスク名', with: :title
+      fill_in 'タスクの説明', with: 'テスト用タスクです'
       fill_in '期限', with: '2020-01-01 00:00:00'
       fill_in 'ステータス', with: '1'
       fill_in '優先度', with: '1'
@@ -30,14 +31,15 @@ describe 'タスク' do
     let(:task) { create(:task) }
     it '変更する' do
       visit edit_task_path(task.id)
-      fill_in 'タスク名', with: '変更タスク1'
+      fill_in 'タスク名', with: '変更タスク'
       click_button '登録'
-      expect(page).to have_content '変更タスク1'
+      expect(Task.exists?(title: '変更タスク')).to be true
+      expect(page).to have_content '変更タスク'
     end
 
     it '更新完了のフラッシュメッセージが表示される' do
       visit edit_task_path(task.id)
-      fill_in 'タスク名', with: '変更タスク2'
+      fill_in 'タスク名', with: '変更タスク'
       click_button '登録'
       expect(page).to have_content 'タスクを更新しました'
     end
@@ -48,8 +50,8 @@ describe 'タスク' do
     it '削除する' do
       visit '/tasks'
       click_link '削除'
-      expect(Task.exists?(title: 'テスト用タスク')).not_to be true
-      expect(page).not_to have_content 'テスト用タスク'
+      expect(Task.exists?(title: :title)).not_to be true
+      expect(page).not_to have_content :title
     end
 
     it '削除完了のフラッシュメッセージが表示される' do
