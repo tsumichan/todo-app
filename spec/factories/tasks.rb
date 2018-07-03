36dae3c63f36afa02d40b0ff106defde55ef55bb
tsumichan <kdy78022@gmail.com>
2018-07-03 17:04:53 +0900

    テストデータの日付をDate.parseで作るようにした

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     title 'テスト用タスク'
     description 'テスト用タスクです'
-    due_at '2020-01-01 00:00:00'
+    due_at Date.parse('2020-01-01')
     status '1'
     priority '1'
   end
