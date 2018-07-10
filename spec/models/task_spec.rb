a30d5a3325f19bd1cdd7883888b5266612f772e6
tsumichan <kdy78022@gmail.com>
2018-07-10 16:17:33 +0900

    とても見やすくなった〜

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,38 +1,39 @@
 require 'rails_helper'
 
-describe 'タスク' do
-  context 'タスクを作成する' do
-    let(:task) { build(:task) }
-    it '正常に保存ができること' do
-      expect(task).to be_valid
-    end
+describe '#title' do
+  let(:task) { build(:task) }
+  it '正常に保存ができること' do
+    expect(task).to be_valid
+  end
 
-    it 'タスク名が空欄とき、保存ができないこと' do
-      task.title = ''
-      task.valid?
-      expect(task.errors.details[:title].any? { |e| e[:error] == :blank }).to be true
-    end
+  it '空欄とき、保存ができないこと' do
+    task.title = ''
+    task.valid?
+    expect(task.errors.details[:title].any? { |e| e[:error] == :blank }).to be true
+  end
 
-    it 'タスク名が100文字以内なら保存ができること' do
-      task.title = 'a' * 100
-      expect(task).to be_valid
-    end
+  it '100文字以内なら保存ができること' do
+    task.title = 'a' * 100
+    expect(task).to be_valid
+  end
 
-    it 'タスク名が101文字以上だと保存ができないこと' do
-      task.title = 'a' * 101
-      task.valid?
-      expect(task.errors.details[:title].any? { |e| e[:error] == :too_long }).to be true
-    end
+  it '101文字以上だと保存ができないこと' do
+    task.title = 'a' * 101
+    task.valid?
+    expect(task.errors.details[:title].any? { |e| e[:error] == :too_long }).to be true
+  end
+end
 
-    it 'タスクの説明文が255文字以内なら保存ができること' do
-      task.description = 'a' * 255
-      expect(task).to be_valid
-    end
+describe '#description' do
+  let(:task) { build(:task) }
+  it '255文字以内なら保存ができること' do
+    task.description = 'a' * 255
+    expect(task).to be_valid
+  end
 
-    it 'タスクの説明文が256文字以上なら保存ができないこと' do
-      task.description = 'a' * 256
-      task.valid?
-      expect(task.errors.details[:description].any? { |e| e[:error] == :too_long }).to be true
-    end
+  it '256文字以上なら保存ができないこと' do
+    task.description = 'a' * 256
+    task.valid?
+    expect(task.errors.details[:description].any? { |e| e[:error] == :too_long }).to be true
   end
 end
