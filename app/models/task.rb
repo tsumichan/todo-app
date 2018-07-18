899e938c60ca0bb62d6228cf518e0185f1d20606
tsumichan <kdy78022@gmail.com>
2018-07-18 12:20:58 +0900

    enumで人間に優しくなった

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :description, length: { maximum: 255 }
+  enum status: { waiting: 0, working:1, completed: 2 }
 
   def self.search(key)
     self.where('title LIKE ?', "%#{key}%")
