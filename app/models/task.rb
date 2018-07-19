a7966062b293967d970156c0e440e8d2e6f6a2c0
tsumichan <kdy78022@gmail.com>
2018-07-19 15:36:24 +0900

    タイトル検索も present?で中身があるときだけWHEREするようにした

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,6 @@ class Task < ApplicationRecord
   validates :description, length: { maximum: 255 }
   enum status: { Todo: 0, Doing: 1, Done: 2 }
 
-  scope :search, ->(word) { where('title LIKE ?', "%#{word}%") }
+  scope :search, ->(word) { where('title LIKE ?', "%#{word}%") if word.present? }
   scope :search_status, ->(status) { where(status: status) if status.present? }
 end
