1c4b87f9a9b009d27f82b63e1343d5001762329b
tsumichan <kdy78022@gmail.com>
2018-07-19 10:30:03 +0900

    目がキョッとしないように半角スペースを入れました

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :description, length: { maximum: 255 }
-  enum status: { Todo: 0, Doing:1, Done: 2 }
+  enum status: { Todo: 0, Doing: 1, Done: 2 }
 
   scope :search, ->(key) { where('title LIKE ?', "%#{key}%") }
   scope :search_status, ->(key) { where(status: key) if key.present? }
