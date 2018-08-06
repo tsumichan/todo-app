51a3b9a731305d3c10f075660b9f46f5f633853c
tsumichan <kdy78022@gmail.com>
2018-08-06 16:42:13 +0900

    get_を消した

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,9 +1,9 @@
 module TasksHelper
-  def get_status_select_options
+  def status_select_options
     Task.statuses.keys.map { |k| [t("enums.task.status.#{k}"), k] }
   end
 
-  def get_priority_select_options
+  def priority_select_options
     Task.priorities.keys.map { |k| [t("enums.task.priority.#{k}"), k] }
   end
 end
