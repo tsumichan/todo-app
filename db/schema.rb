c55dc13b898a5b4e450016714e51a7fe4c9a8b79
tsumichan <kdy78022@gmail.com>
2018-08-08 18:07:24 +0900

    passwordをpassword_digestへ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_091047) do
+ActiveRecord::Schema.define(version: 2018_08_08_090532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,10 +30,10 @@ ActiveRecord::Schema.define(version: 2018_08_06_091047) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "tasks", "users"
