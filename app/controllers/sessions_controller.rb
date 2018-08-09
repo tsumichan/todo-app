f5e11bebdb53737e080c1b6fa0369744f6c82b2e
tsumichan <kdy78022@gmail.com>
2018-08-09 12:51:14 +0900

    ログイン処理とログアウト処理をheplerに書いた

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:session][:name].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in(user)
       redirect_to root_path
     else
       flash[:danger] = 'ログインに失敗しました'
@@ -13,5 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to login_path
   end
 end
