70b4f819b86c3ff78b224a781ee8155c55d08aa4
tsumichan <kdy78022@gmail.com>
2018-08-09 15:43:19 +0900

    自分のタスクだけが表示されるように

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
   def index
     @statuses = Task.statuses.map { |k, v| [t("enums.task.status.#{k}"), v]}
     @sorts = [t('views.task.sort.created_at'), 0], [t('views.task.sort.due_at'), 1], [t('views.task.sort.priority_desc'), 2], [t('views.task.sort.priority_asc'), 3]
-    @tasks = Task.includes(:user).search_by_title(params[:search]).search_by_status(params[:status]).order_by(params[:sort]).page(params[:page])
+    @tasks = Task.includes(:user).where(user_id: @current_user.id).search_by_title(params[:search]).search_by_status(params[:status]).order_by(params[:sort]).page(params[:page])
   end
 
   def new
@@ -13,8 +13,8 @@ class TasksController < ApplicationController
   end
 
   def create
-    # ログイン処理がまだないので、User.tasks から task を作る
-    @task = User.last.tasks.build(task_params)
+    @task = Task.new(task_params)
+    @task.id = @current_user.id
     if @task.save
       redirect_to root_path, flash: { success: t('views.task.message.created') }
     else
