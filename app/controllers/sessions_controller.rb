48ee088351b2edbfdc7c6e75eb02b683a5f3eebe
tsumichan <kdy78022@gmail.com>
2018-08-09 14:15:09 +0900

    flashもi18nしよ〜

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,15 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:session][:name].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in(user)
-      redirect_to root_path
+      redirect_to root_path, flash: { success: t('views.user.message.log_in_success') }
     else
-      flash[:danger] = 'ログインに失敗しました'
+      flash[:danger] = t('views.user.message.log_in_failed')
       render :new
     end
   end
 
   def destroy
     log_out
-    redirect_to login_path
+    redirect_to login_path, flash: { success: t('views.user.message.log_out_success') }
   end
 end
