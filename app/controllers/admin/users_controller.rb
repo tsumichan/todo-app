72abf4b379fad8b7478057697f6c3b43331549c9
tsumichan <kdy78022@gmail.com>
2018-09-03 11:55:25 +0900

    reject_visitor_access も before_action で実行する

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < ApplicationController
-  before_action :reject_common_access
+  before_action :reject_visitor_access, :reject_common_access
 
   def index
     @users = User.all.page(params[:page])
