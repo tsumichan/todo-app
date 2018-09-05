60e0c5e7a61f9c36d4af093b966da8fc1030b3f8
tsumichan <kdy78022@gmail.com>
2018-09-05 11:36:30 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :tasks, dependent: :destroy
   enum role: { common: 0, admin: 1 }
+
+  validates :name, presence: true, length: { minimum: 5, maximum: 25 }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum:8, maximum: 100 }
+  validates :role, presence: true
 end
