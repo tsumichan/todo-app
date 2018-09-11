1389bbedcf608df40eec84ed46c56c9035c22a1f
tsumichan <kdy78022@gmail.com>
2018-09-11 15:06:27 +0900

    counter_cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_08_090532) do
+ActiveRecord::Schema.define(version: 2018_09_10_082738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_08_08_090532) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.integer "tasks_count"
   end
 
   add_foreign_key "tasks", "users"
