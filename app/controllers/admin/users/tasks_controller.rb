c2e3054e39c852f2ba629d78d5f4a2809fc793c5
tsumichan <kdy78022@gmail.com>
2018-09-13 14:56:19 +0900

    .tasks でシュッとする

--- app/controllers/admin/users/tasks_controller.rb
+++ app/controllers/admin/users/tasks_controller.rb
@@ -2,7 +2,6 @@ class Admin::Users::TasksController < ApplicationController
   before_action :reject_visitor_access, :reject_common_access
 
   def index
-    user = User.find(params[:user_id])
-    @tasks = Task.where(user_id: user.id).page(params[:page])
+    @tasks = User.find(params[:user_id]).tasks.page(params[:page])
   end
 end
