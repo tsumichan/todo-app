8b666f583a0970c97f24f24737bece4d0c89526a
tsumichan <kdy78022@gmail.com>
2018-09-13 14:42:28 +0900

    user を find してから where する

--- app/controllers/admin/users/tasks_controller.rb
+++ app/controllers/admin/users/tasks_controller.rb
@@ -2,6 +2,7 @@ class Admin::Users::TasksController < ApplicationController
   before_action :reject_visitor_access, :reject_common_access
 
   def index
-    @tasks = Task.where(user_id: params[:user_id]).page(params[:page])
+    user = User.find(params[:user_id])
+    @tasks = Task.where(user_id: user.id).page(params[:page])
   end
 end
