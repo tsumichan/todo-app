c08419fd5dc64599f26d40592b640e221eda83e6
tsumichan <kdy78022@gmail.com>
2018-09-18 15:13:05 +0900

    なぜかインデント下がっていた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,16 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   private
-    def set_locale
-      I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
-    end
+  
+  def set_locale
+    I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
+  end
 
-    def render_404_if_common_accessed
-      render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html' unless current_user&.admin?
-    end
+  def render_404_if_common_accessed
+    render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html' unless current_user&.admin?
+  end
 
-    def reject_visitor_access
-      redirect_to login_path, flash: { warning: t('views.user.message.require_login') } unless logged_in?
-    end
+  def reject_visitor_access
+    redirect_to login_path, flash: { warning: t('views.user.message.require_login') } unless logged_in?
+  end
 end
