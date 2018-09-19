7a86ccd814f992608ea1d92f3d2ec60cdf3472b7
tsumichan <kdy78022@gmail.com>
2018-09-19 18:13:11 +0900

    controller 書いた

---
+++ app/controllers/setting/labels_controller.rb
@@ -0,0 +1,25 @@
+class Setting::LabelsController < ApplicationController
+  before_action :reject_visitor_access
+  before_action :set_label, only: [:edit, :update, :destroy]
+  def new
+    @label = Label.new
+  end
+
+  def create
+    @label = Label.new(label_params)
+    if @label.save
+      redirect_to setting_labels_path, flash: { success: t('views.task.message.created') }
+    else
+      render :new
+    end
+  end
+  private
+
+  def label_params
+    params.require(:label).permit(:name)
+  end
+
+  def set_label
+    @label = Label.find(params[:id])
+  end
+end
