8fc1c3f5b516d0651d8b652d6e4ac20347183f20
tsumichan <kdy78022@gmail.com>
2018-09-20 17:50:40 +0900

    ラベル名がかぶっても保存ができるように

--- app/models/label.rb
+++ app/models/label.rb
@@ -3,5 +3,5 @@ class Label < ApplicationRecord
   has_many :tasks, through: :task_labels
   belongs_to :user
 
-  validates :name, presence: true, uniqueness: true, length: { minimum: 1, maximum: 15 }
+  validates :name, presence: true, length: { minimum: 1, maximum: 15 }
 end
