7a61f44bf6fd978707d8edc23df1ae3774e5bed4
tsumichan <kdy78022@gmail.com>
2018-09-25 18:31:05 +0900

    モデルバリデーションがうまくいってなかった…

--- app/models/task_label.rb
+++ app/models/task_label.rb
@@ -2,5 +2,5 @@ class TaskLabel < ApplicationRecord
   belongs_to :task
   belongs_to :label
 
-  validates :task_id, uniqueness: { scope: :label_id }
+  validates_uniqueness_of :task_id, scope: :label_id
 end
--- spec/models/task_label_spec.rb
+++ spec/models/task_label_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 describe 'TaskLabel' do
-  let(:task) { build(:task) }
+  let!(:label) { create(:label) }
+  let!(:task) { create(:task, label_ids: [label.id]) }
   it '重複したレコードが保存できないこと' do
-    other_task = Task.new(id: task.id, label_ids: [task.labels.first.id])
-    other_task_label.valid?
-    expect(other_task_label.errors.details[:task_id].any? { |e| e[:error] == :taken }).to be true
+    other_task = Task.new(id: task.id, user_id: task.user_id, title: task.title, label_ids: [task.labels.last.id])
+    other_task.valid?
+    expect(other_task.errors.details[:task_labels].any? { |e| e[:error] == :invalid }).to be true
   end
 end
