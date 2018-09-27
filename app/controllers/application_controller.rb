16c41de266c984f6aa74d936156ffb8403b51e9f
tsumichan <kdy78022@gmail.com>
2018-09-27 15:55:56 +0900

    404と500のエラーを表示するようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,26 @@
 class ApplicationController < ActionController::Base
+  rescue_from StandardError, with: :render_500 unless Rails.env.development?
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404 unless Rails.env.development?
+  rescue_from ActionController::RoutingError, with: :render_404 unless Rails.env.development?
   include Session
   helper_method :logged_in?
   before_action :set_locale
+
+  def render_404(e = nil)
+    if e
+      logger.error e
+      logger.error e.backtrace.join("\n")
+    end
+    render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+  end
+
+  def render_500(e = nil)
+    if e
+      logger.error e
+      logger.error e.backtrace.join("\n")
+    end
+    render file: Rails.root.join('public/500.html'), status: 500, layout: false, content_type: 'text/html'
+  end
 
   private
   
