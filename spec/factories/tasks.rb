e544b44cdd5c2a627a9411b6c3ec8c56d5874a56
tsumichan <tsumichan@pepabo.com>
2019-03-12 20:47:26 +0900

    推奨される記法に直した

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,26 +1,26 @@
 FactoryBot.define do
   factory :task do
-    title 'テスト用タスク'
-    description 'テスト用タスクです'
-    due_at Date.parse('2020-01-01')
-    status 'todo'
-    priority 'nothing'
+    title { 'テスト用タスク' }
+    description { 'テスト用タスクです' }
+    due_at { Date.parse('2020-01-01') }
+    status { 'todo' }
+    priority { 'nothing' }
     user { create(:user) }
 
     factory :new_task do
-      created_at Date.parse('2020-01-01')
+      created_at { Date.parse('2020-01-01') }
     end
 
     factory :old_task do
-      created_at Date.parse('2000-01-01')
+      created_at { Date.parse('2000-01-01') }
     end
 
     factory :high_priority_task do
-      priority 'high'
+      priority { 'high' }
     end
 
     factory :low_priority_task do
-      priority 'low'
+      priority { 'low' }
     end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,19 +1,19 @@
 FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "user#{n}"}
-    password 'password'
-    role 1
+    password { 'password' }
+    role { 1 }
 
     factory :admin_user do
       sequence(:name) { |n| "admin_user#{n}"}
-      password 'password'
-      role 1
+      password { 'password' }
+      role { 1 }
     end
 
     factory :common_user do
       sequence(:name) { |n| "common_user#{n}"}
-      password 'password'
-      role 0
+      password { 'password' }
+      role { 0 }
     end
   end
 end
